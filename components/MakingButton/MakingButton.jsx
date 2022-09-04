18dd181a32c64d7252fa787d8f8cb35d899e8fd8
yrakozlov <yurakozlov0995508414@gmail.com>
2022-09-04 14:35:18 +0300

    first commit

---
+++ components/MakingButton/MakingButton.jsx
@@ -0,0 +1,7 @@
+import styles from './Making.module.scss';
+
+const MakingButton = () => {
+	return <div className={styles.makingButton}>MakingButton</div>;
+};
+
+export default MakingButton;
---
+++ pages/index.js
@@ -0,0 +1,11 @@
+import MakingButton from '../components/MakingButton/MakingButton';
+
+const index = () => {
+	return (
+		<div>
+			index <MakingButton />
+		</div>
+	);
+};
+
+export default index;
